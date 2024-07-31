c8871374797bbb2add66fc09ac9e88ebc19a3f14
Ricky Jun <djoeniar@gmail.com>
2024-07-31 14:35:55 +0700

    fiz auto update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,58 @@
-const CACHE_NAME = 'badminton-pair-randomizer-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png',
-  '/favicon.ico',
-  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('badminton-pair-randomizer-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+        '/favicon.ico',
+        'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        fetch(event.request).then((response) => {
+          return caches.open('badminton-pair-randomizer-v1').then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+        return response;
+      }
+
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
-        return fetch(event.request);
-      })
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open('badminton-pair-randomizer-v1').then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
   );
 });
